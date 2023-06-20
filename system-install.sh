d78b3bd81b9a17b25064ea74b7ba85d6e621e41c
hle <hoang.b.le.au@gmail.com>
2023-06-21 00:04:14 +1000

    feat: start and enable bluetooth

--- system-install.sh
+++ system-install.sh
@@ -1,5 +1,8 @@
 #bluetooth
 sudo pacman -S bluez bluez-utils blueman
+insmod filename btusb
+systemctl start bluetooth.service
+systemctl enable bluetooth.service
 
 #graphics
 sudo pacman -S mesa glmark2
