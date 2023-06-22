885d3fe5f379e82c11d73feed0870c6b00c5268c
hle <hoang.b.le.au@gmail.com>
2023-06-23 00:33:15 +1000

    feat: add i3 stow config

---
+++ i3.sh
@@ -0,0 +1,10 @@
+#i3 wm and nitrogen 
+paru -S stow i3 i3ipc-glib-git xfce4-i3-workspaces-plugin-git nitrogen
+
+stow i3
+
+#kill xfcewm4 and xfdesktop using graphical interface]
+#add i3-workspaces-plugin to the panel items
+#add i3 command to startup session
+#remove all keybindings
+#add nitrogen --restore to startup session
--- pkg-to-install.sh
+++ pkg-to-install.sh
@@ -1,51 +1,42 @@
 sudo pacman -Syyu
 
-sudo pacman -S git firefox rustup go python nix direnv fish docker btop libreoffice-fresh
+sudo pacman -S git base-devel opera rustup go python nix direnv fish docker btop libreoffice-fresh
 
-mkdir i
-cd i
+mkdir .i
+cd .i
 
 git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 cd ..
 
-
+# maybe yay if u'd like
 # git clone https://aur.archlinux.org/yay.git
 # cd yay
 # makepkg -si
 # cd ..
 
 
-
 #browsers + api
-# sudo pacman -S firefox
-paru -S google-chrome
-paru -S postman 
+paru -S google-chrome postman
 
 #code, rust, etc.
 # sudo pacman -S rustup
 rustup install stable
 rustup default stable
-
-
-# sudo pacman -S go
-
-# sudo pacman -S python
 python -m ensurepip --upgrade
 
-# sudo pacman -S btop
 
 
-#code env	
+#code env
 # sudo pacman -S docker
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
 
-paru -S visual-studio-code-bin github-cli aws-cli-git 
+paru -S visual-studio-code-bin github-cli aws-cli-git fish
 paru -S nix
 
 
-#out of /i
+#out of /.i
 cd ..
